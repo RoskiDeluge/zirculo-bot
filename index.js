58a310606584ae2e44175cf8c8813fffb30a7862
Roberto Delgado <roberto.delgado@gmail.com>
2017-08-27 06:22:31 +0000

    Second commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const express = require('express');
 const Twit = require('twit');
+const config = require('./config.js');
+const app = express();
 
 /*
   HEY
@@ -9,37 +12,53 @@ const Twit = require('twit');
 
   k
 */
-const config = require('./config');
+
 const T = new Twit(config);
 
-const niceThingsToSay = [
-  'It\'s Operator Mono! He wrote a little more about his setup at http://WesBos.com/uses',
-  'Hey There! Operator Mono is the name of that game http://WesBos.com/uses',
-  'Operator Mono! http://WesBos.com/uses',
-  'At your service! It\'s operator mono! http://WesBos.com/uses',
-  'wait for it................  operator mono! → → → →  http://WesBos.com/uses',
-  'Hey! check out http://WesBos.com/uses - it\'s Operator Mono',
-];
+/* const niceThingsToSay = [
+  'It\'s Zirculo, RD\'s bot! He wrote a little more about his ideas at https://roskideluge.github.io/',
+  'Hey There! The eclipse was amazing, check this out https://roskideluge.github.io/',
+  'Zirculo is about circles! https://roskideluge.github.io/',
+  'At your service! It\'s Zirculo! https://roskideluge.github.io/',
+  'wait for it................  Zirculo! → → → →  https://roskideluge.github.io/',
+  'Hey! check out https://roskideluge.github.io/ - it\'s RD\'s manifesto.',
+]; */
 
-const funEmojis = ['👊','🔥','👍','🎉','💁','🙃','🍕','😎','😘','👏','✌️','👌','👈','👙','🐷','🍟'];
+// const funEmojis = ['👊','🔥','👍','🎉','💁','🙃','🍕','😎','😘','👏','✌️','👌','👈','👙','🐷','🍟'];
 
-function buildTweet() {
+/* function buildTweet() {
   // get a random emoji
   const emoji = funEmojis[Math.floor(Math.random() * funEmojis.length)];
   // get a random sentence
   const sentence = niceThingsToSay[Math.floor(Math.random() * niceThingsToSay.length)];
   return `${sentence} ${emoji}`;
-}
+} */
 
-const stream = T.stream('statuses/filter', { track: 'wesbos font' });
+const stream = T.stream('statuses/filter', {
+  track: 'eternal recurrence, eterno retorno' 
+});
 
 stream.on('tweet', (tweet) => {
   console.log(tweet);
-  const createdTweet = {
+  /* const createdTweet = {
     status: `@${tweet.user.screen_name} ${buildTweet()}`,
     in_reply_to_status_id: tweet.id_str
-  };
-  T.post('statuses/update', createdTweet, (err, response) => {
-    console.log(response);
+  }; */
+  
+  /* Changed it so that it automatically RT any tweet with the 
+  tracked words, "eternal recurrence, eterno retorno" - RD */
+  T.post('statuses/retweet/:id', {
+    id: tweet.id_str
+  }, (err, data, response) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('${data.text}')
+    }
   });
 });
+
+//Function is set to trigger RT every 5 min. 
+setInterval(T.stream, 300000);
+
+app.listen(3000, () => console.log('Server listening on port 3000!') );
